dd4a673f79d3d01ba6a19c67d4c97dd1e0893a93
iheb-ferjani <mohamediheb.ferjani@esprit.tn>
2020-12-29 21:44:03 +0100

    Created Serie and Comments entities

--- back-end/src/main/java/com/project/entities/Serie.java
+++ back-end/src/main/java/com/project/entities/Serie.java
@@ -1,5 +1,77 @@
 package com.project.entities;
 
-public class Serie {
+import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+@Entity
+@Table(name="series")
+public class Serie implements Serializable{
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	private String name;
+	
+	public Serie() {
+		super();
+	}
+	
+	public Serie(String name) {
+		super();
+		this.name = name;
+	}
+
+	public Serie(long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Serie other = (Serie) obj;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	
+	
+	
 }
