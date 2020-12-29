0816e4a202f1a4f4a0934746a457c10ef3820ff8
yassineomranii <yassine.omrani@esprit.tn>
2020-12-29 22:17:32 +0100

    also entities

--- back-end/src/main/java/com/project/entities/FavoriteAuthor.java
+++ back-end/src/main/java/com/project/entities/FavoriteAuthor.java
@@ -1,12 +1,15 @@
 package com.project.entities;
 
+import java.io.Serializable;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name="favoriteAuthors")
-public class FavoriteAuthor {
+public class FavoriteAuthor implements Serializable{
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
+	
 }
--- back-end/src/main/java/com/project/entities/User.java
+++ back-end/src/main/java/com/project/entities/User.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import net.bytebuddy.dynamic.loading.InjectionClassLoader.Strategy;
 
@@ -31,6 +32,8 @@ public class User implements Serializable{
 	private String role;
 	
 	private String picUrl;
+	
+	
 
 	public long getId() {
 		return id;
