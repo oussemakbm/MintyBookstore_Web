a9636ef41277181e14ac20034ec5a827295bcc13
yassineomranii <yassine.omrani@esprit.tn>
2020-12-29 21:44:55 +0100

    draft hani raja3

--- back-end/src/main/java/com/project/entities/User.java
+++ back-end/src/main/java/com/project/entities/User.java
@@ -1,17 +1,188 @@
 package com.project.entities;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
+import net.bytebuddy.dynamic.loading.InjectionClassLoader.Strategy;
 
 
-
-@Entity
 @Table(name="users")
-public class User {
+@Entity
+public class User implements Serializable{
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private long id;
+	
 	private String name;
+	
 	private String email;
+	
+	private String password;
+	
+	private String numTel;
+	
+	private String adresse;
+	
+	private String role;
+	
+	private String picUrl;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getNumTel() {
+		return numTel;
+	}
+
+	public void setNumTel(String numTel) {
+		this.numTel = numTel;
+	}
+
+	public String getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getPicUrl() {
+		return picUrl;
+	}
+
+	public void setPicUrl(String picUrl) {
+		this.picUrl = picUrl;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", email=" + email + ", password=" + password + ", numTel="
+				+ numTel + ", adresse=" + adresse + ", role=" + role + ", picUrl=" + picUrl + "]";
+	}
+
+	public User() {
+		super();
+	}
+
+	public User(long id, String name, String email, String password, String numTel, String adresse, String role,
+			String picUrl) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.numTel = numTel;
+		this.adresse = adresse;
+		this.role = role;
+		this.picUrl = picUrl;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((adresse == null) ? 0 : adresse.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((numTel == null) ? 0 : numTel.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((picUrl == null) ? 0 : picUrl.hashCode());
+		result = prime * result + ((role == null) ? 0 : role.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (adresse == null) {
+			if (other.adresse != null)
+				return false;
+		} else if (!adresse.equals(other.adresse))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (numTel == null) {
+			if (other.numTel != null)
+				return false;
+		} else if (!numTel.equals(other.numTel))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (picUrl == null) {
+			if (other.picUrl != null)
+				return false;
+		} else if (!picUrl.equals(other.picUrl))
+			return false;
+		if (role == null) {
+			if (other.role != null)
+				return false;
+		} else if (!role.equals(other.role))
+			return false;
+		return true;
+	}
+	
 	
 
 }
