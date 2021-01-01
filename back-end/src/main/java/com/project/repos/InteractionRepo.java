ab35765549a6e55d775bb34410d9f5cce72358fa
yassineomranii <yassine.omrani@esprit.tn>
2021-01-01 16:52:09 +0100

    add RepoMethods

--- back-end/src/main/java/com/project/repos/InteractionRepo.java
+++ back-end/src/main/java/com/project/repos/InteractionRepo.java
@@ -1,9 +1,18 @@
 package com.project.repos;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.project.entities.Interaction;
 
 public interface InteractionRepo extends CrudRepository<Interaction, Long> {
 		
+	@Query("Update i set i.liked = 1 WHERE i.id = commentId ")
+	public void likeBook(long BookId); 
+	
+	@Query("Update i set i.liked = 0 WHERE i.id = commentId ")
+	public void unlikeBook(long BookId);
+	
+	
+	
 }
