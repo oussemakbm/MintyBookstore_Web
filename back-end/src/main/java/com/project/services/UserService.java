1f0574e55bd53d95d008f34f4b74cfa9d97154b9
oussemakbm <khalifaoussema.benmimouna@esprit.tn>
2021-01-04 01:35:00 +0100

    update application.properties & implements methods FavoritesSeries

--- back-end/src/main/java/com/project/services/UserService.java
+++ back-end/src/main/java/com/project/services/UserService.java
@@ -1,5 +1,18 @@
 package com.project.services;
 
-public class UserService {
+import java.util.List;
 
+import com.project.entities.Serie; 
+
+public interface UserService {
+	
+	
+	/* Favorites Series */
+	public void addToFavoriteSerie(Long user_id,Long serie_id);
+	
+	public void deleteFromFavoriteSerie(Long user_id,Long serie_id);
+	
+	public List<Serie> getAllFavoriteSeries(Long user_id);
+
+	public Serie getFavoriteSerie(Long user_id,Long serie_id);
 }
---
+++ back-end/src/main/java/com/project/services/UserServiceImpl.java
@@ -0,0 +1,38 @@
+package com.project.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+
+import com.project.entities.Serie;
+import com.project.repos.UserRepo;
+
+public class UserServiceImpl implements UserService{
+	
+	@Autowired
+	UserRepo userRepo;
+
+	/* Favorites Series */
+	@Override
+	public void addToFavoriteSerie(Long user_id,Long serie_id) {
+		userRepo.addToFavoriteSerie(user_id,serie_id);
+	}
+
+	@Override
+	public void deleteFromFavoriteSerie(Long user_id,Long serie_id) {
+		userRepo.deleteFromFavoriteSerie(user_id,serie_id);		
+	}
+
+	@Override
+	public List<Serie> getAllFavoriteSeries(Long user_id) {
+		return userRepo.getAllFavoriteSeries(user_id);
+	}
+
+	@Override
+	public Serie getFavoriteSerie(Long user_id,Long serie_id) {
+		// TODO Auto-generated method stub
+		return	getFavoriteSerie(user_id, serie_id);
+	}
+
+}
