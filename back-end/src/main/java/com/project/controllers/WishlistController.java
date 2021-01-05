a240e25950be6a87a6c10281ee7f6a851d7ed0e6
iheb-ferjani <mohamediheb.ferjani@esprit.tn>
2021-01-05 22:58:20 +0100

    Preparing Wishlist Crud

---
+++ back-end/src/main/java/com/project/controllers/WishlistController.java
@@ -0,0 +1,8 @@
+package com.project.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class WishlistController {
+
+}
---
+++ back-end/src/main/java/com/project/services/WishlistServiceImpl.java
@@ -0,0 +1,5 @@
+package com.project.services;
+
+public class WishlistServiceImpl implements WishlistService{
+
+}
