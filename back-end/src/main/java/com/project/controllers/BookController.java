994a50809e24c78d83f7956508023f76b4131e3e
oussemakbm <khalifaoussema.benmimouna@esprit.tn>
2021-01-22 21:25:42 +0100

    create bookController & serieController

--- back-end/src/main/java/com/project/controllers/BookController.java
+++ back-end/src/main/java/com/project/controllers/BookController.java
@@ -1,5 +1,13 @@
 package com.project.controllers;
 
-public class BookController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.project.services.BookServiceImpl;
+
+@RestController
+public class BookController {
+	
+	@Autowired
+	BookServiceImpl bookservice;
 }
