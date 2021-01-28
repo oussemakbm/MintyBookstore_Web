66702a840f8c06966367ba7ccc9f6e69fa2cdbdd
oussemakbm <khalifaoussema.benmimouna@esprit.tn>
2021-01-28 01:28:25 +0100

    update BookController and application.properties

--- back-end/src/main/java/com/project/controllers/BookController.java
+++ back-end/src/main/java/com/project/controllers/BookController.java
@@ -1,9 +1,19 @@
 package com.project.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.services.BookServiceImpl;
+
+import com.project.entities.Book;
+
 
 @RestController
 public class BookController {
@@ -11,16 +21,29 @@ public class BookController {
 	@Autowired
 	BookServiceImpl bookservice;
 	
-	/*addBook
-	postmapping
-	bookservice*/
-
-	/*editBook
-	postmapping
-	bookservice*/
-
-	/*deleteBook
-	postmapping
-	bookservice*/
+	// http://localhost:8081/MintyBook/servlet/addBook
+	@PostMapping("/addBook")
+	public Book addBook(@RequestBody Book book){
+		bookservice.addOrUpdateBook(book);
+		return book;
+	}
+	
+	// http://localhost:8081/MintyBook/servlet/updateBook
+	@PutMapping(value="/updateBook")
+	public void updateBook(@RequestBody Book book){
+		bookservice.addOrUpdateBook(book);
+	}
+	
+	// http://localhost:8081/tp-timesheet/servlet/getBooks
+	@PostMapping("/getBooks")
+	public List<Book> getBooks(){
+		return bookservice.getBooks();
+	}
+	
+	// http://localhost:8081/tp-timesheet/servlet/deleteBook
+	@DeleteMapping("/deleteBook/{idbook}")
+	public void deleteBook(@PathVariable("idbook") long bookid){
+		bookservice.deleteById(bookid);
+	}
 
 }
