24f0a6b6d3b259337673d769564370b59eb245d8
yassineomranii <yassine.omrani@esprit.tn>
2021-02-01 22:13:02 +0100

    some added features

---
+++ back-end/src/main/java/com/project/DTOs/CommentDTO.java
@@ -0,0 +1,16 @@
+package com.project.DTOs;
+
+public class CommentDTO {
+	
+	private String body;
+	
+	
+	public String getBody(){
+		return this.body;
+	}
+	
+	public void setBody(String body){
+		this.body = body;
+	}
+	
+}
