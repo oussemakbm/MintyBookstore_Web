e90e6f3b7a91d991b4a559e9dbeb6fc732363526
ach-1027 <achraf.fakhfakh@esprit.tn>
2021-02-08 01:17:46 +0100

    exemple

--- back-end/src/main/java/com/project/services/AuthorPreferServiceImpl.java
+++ back-end/src/main/java/com/project/services/AuthorPreferServiceImpl.java
@@ -1,6 +1,7 @@
 package com.project.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,31 +19,42 @@ public class AuthorPreferServiceImpl implements AuthorPreferService {
     @Autowired
     AuthorRepo authorRepo ;
 	@Override
-	public void addAuthorPrefer( Long user_id, Long author_id) {
+	public boolean addAuthorPrefer( long user_id, long author_id) {
+		if(!(authorRepo.existsById(user_id) && authorRepo.existsById(author_id)))
+			return false;
 		Author author = authorRepo.findById(author_id).get();
 		User user = userRepo.findById(user_id).get();
 		user.getFavoriteAuthors().add(author);
-		
+		return true;
 	}
 
 	@Override
-	public void deleteAuthorPrefer(Long user_id, Long author_id) {
+	public boolean deleteAuthorPrefer(long user_id, long author_id) {
+		if(!(userRepo.existsById(user_id)))
+			return false;
 		Author author = authorRepo.findById(author_id).get();
 		User user = userRepo.findById(author_id).get();
 		user.getFavoriteAuthors().remove(author);	
-		
+		userRepo.save(user);
+		return true;
 	}
 
 	@Override
-	public List<Author> getAllPreferAuthor(Long user_id) {
+	public List<Author> getAllPreferAuthor(long user_id) {
+		if(!(userRepo.existsById(user_id)))
+			return null;
 		User user = userRepo.findById(user_id).get();
 		return user.getFavoriteAuthors();
 	}
 
 	@Override
-	public Author getPreferAuthor(Long user_id, Long author_id) {
-		Author author = authorRepo.findById(author_id).get();
+	public List<Author> findPreferAuthorByName (long user_id, String name) {
+		if(!(userRepo.existsById(user_id)))
+			return null;
 		User user = userRepo.findById(user_id).get();
-		return authorRepo.findById(author_id).get();
+		List <Author> authors = user.getFavoriteAuthors().stream()
+				.filter(a -> a.getName().toUpperCase().contains(name.toUpperCase())).collect(Collectors.toList());
+		return authors;
 	}
-}
+	}
+
