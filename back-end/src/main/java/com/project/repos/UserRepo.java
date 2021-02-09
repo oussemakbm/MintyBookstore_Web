2bc80dbd68a600fe42b6f6a30edda2abc842e244
iheb-ferjani <mohamediheb.ferjani@esprit.tn>
2021-02-09 23:42:13 +0100

    Updating User repository

--- back-end/src/main/java/com/project/repos/UserRepo.java
+++ back-end/src/main/java/com/project/repos/UserRepo.java
@@ -20,4 +20,43 @@ public interface UserRepo extends CrudRepository<User, Long>{
 	@Query("Select c FROM CommandList c WHERE c.user.id = :userId")
 	List<CommandList> getUserCommandList(long userId);
 	
+	
+	/*------------- User Management by admin --------*/
+	
+	@Query("Select u FROM User u WHERE (UPPER(u.username) like UPPER(:search)) OR (UPPER(u.email) like UPPER(:search))"
+			+ " OR (u.numTel like :search) OR (UPPER(u.adresse) like UPPER(:search)) "
+			+ "OR (UPPER(u.name) like UPPER(:search))")
+	List<User> getUsers(String search);
+	
+	
+	
+	@Query("Select u FROM User u ORDER BY u.username")
+	List<User> getUsersByUsernameAsc();
+	
+	@Query("Select u FROM User u ORDER BY u.username DESC")
+	List<User> getUsersByUsernameDesc();
+	
+	
+	@Query("Select u FROM User u ORDER BY u.email")
+	List<User> getUsersByEmailAsc();
+	
+	@Query("Select u FROM User u ORDER BY u.email DESC")
+	List<User> getUsersByEmailDesc();
+	
+	
+	@Query("Select u FROM User u ORDER BY u.name")
+	List<User> getUsersByNameAsc();
+	
+	@Query("Select u FROM User u ORDER BY u.name DESC")
+	List<User> getUsersByNameDesc();
+	
+	
+	@Query("Select u FROM User u ORDER BY u.role")
+	List<User> getUsersByRoleAsc();
+	
+	@Query("Select u FROM User u ORDER BY u.role DESC")
+	List<User> getUsersByRoleDesc();
+	
+	
+	
 }
